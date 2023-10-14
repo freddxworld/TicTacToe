db41884e784cf0d8f49d09bd34428f485614e70b
freddxworld <98077586+freddxworld@users.noreply.github.com>
2023-10-13 18:54:11 -0700

    so we finished the main class, at this point we just need to finish a few more things like the ai and pass the winner then were done

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 #this will run the game
 from board import Board
 from game import Game
-game_board = Board()
-#this is how to create an instance of the board to use in the program
-table = game_board.board
-game_func = Game(table)
-game_board.display()
-while not game_func.is_board_full():
-    game_func.turns()
+
+def main():
+    #this will run the game
+    game_board = Board()
+    table = game_board.board
+
+    game_func = Game(table)
     game_board.display()
-    if game_func.check_win():
-        print("yahh someone won")
-        break
-    if game_func.is_board_full():
-        print("Tie you both lost")
-        break
+
+    while not game_func.is_board_full():
+        game_func.turns()
+        game_board.display()
+
+        if game_func.check_win():
+            print("yahh someone won")
+            break
+        if game_func.is_board_full():
+            print("Tie you both lost")
+            break
+if __name__ == "__main__":
+    main()
